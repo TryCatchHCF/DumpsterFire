5e437643cb91caab87243feeddb1dea6857053bc
TryCatchHCF <TryCatchHCF@users.noreply.github.com>
2017-10-16 14:23:09 -0700

    Add files via upload

--- FireModules/Shenanigans/osx_rickroll_recite_lyrics.py
+++ FireModules/Shenanigans/osx_rickroll_recite_lyrics.py
@@ -26,27 +26,23 @@ class osx_rickroll_recite_lyrics( FireModule ):
 
 	def __init__(self):
 		self.commentsStr = "Shenanigans/osx_rickroll_recite_lyrics"
-		self.textToSayStr = ""
 
 	def __init__(self, moofStr):
 		self.moofStr = moofStr
 		self.commentsStr = "Shenanigans/osx_rickroll_recite_lyrics"
-		self.textToSayStr = ""
 		return;
 
 	def Description( self ):
-		self.Description = "Runs OSX terminal's 'say' command, speaks lyrics from Rick Astley's Never Gonna Give You Up"
+		self.Description = "Runs OSX terminal's 'say' command, speaks lyrics from Rick Astley's Never Gonna Give You Up. After tunrning system volume up to maximum."
 		return self.Description
 
         def Configure( self ):
-                self.textToSayStr = raw_input( "Text to speak: " )
                 return
 
         def GetParameters( self ):
-                return( self.textToSayStr )
+                return( "" )
 
         def SetParameters( self, parametersStr ):
-                self.textToSayStr = parametersStr
                 return
 
         def ActivateLogging( self, logFlag ):
@@ -56,15 +52,32 @@ class osx_rickroll_recite_lyrics( FireModule ):
 
         def Ignite( self ):
 
-                if ( self.textToSayStr == "" ):
-                        print "## ", self.commentsStr, ": Error - Speech string is blank"
-                        return
+		print self.commentsStr + ": Setting system audio to max volume"
+		os.system( "osascript -e 'set volume output volume 100'")
 
-		else:
-			print self.commentsStr + ": Speaking the lyrics from Rick Astley's Never Gonna Give You Up"
-			self.textToSayStr = ""
-			self.sayStr = "say " + self.textToSayStr 
-			os.system( self.sayStr )
+		print self.commentsStr + ": Speaking the lyrics from Rick Astley's Never Gonna Give You Up"
+
+		self.sayStr = "say " + "Never gonna give you up"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "Never gonna let you down"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "Never gonna run around and desert you"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "Never gonna make you cry"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "Never gonna say goodbye"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "Never gonna tell a lie"
+		os.system( self.sayStr )
+
+		self.sayStr = "say " + "And hurt you"
+		os.system( self.sayStr )
 
 		return
+
 
